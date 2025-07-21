9a6ae34ad37275b6e6482e8ef821bca67d378981
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2025-07-21 14:59:34 +0200

    feat: expand AppController with additional CRUD operations and parameter handling

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, HttpCode } from '@nestjs/common';
+import { Controller, Delete, Get, Header, HttpCode, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('houssam')
@@ -12,4 +12,37 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get(':id')
+  findopneParames(@Param() parames: any): string {
+    return `Hello ${parames.id}`;
+  }
+
+
+  @Get(':id')
+  findopneById(@Param() id: string): string {
+    return `Hello ${id}`;
+  }
+
+  @Post()
+  create(@Param() body: any): string {
+    return `Hello ${body}`;
+  }
+  @Post(':id')
+  createById(@Param() id: string): string {
+    return `Hello ${id}`;
+  }
+  @Post(':id/:name')
+  createByIdAndName(@Param() params: any): string {
+    return `Hello ${params.id} and ${params.name}`;
+  }
+  @Put(':id')
+  updateById(@Param() id: string): string {
+    return `update ${id}`;
+  }
+  @Delete(':id')
+  deleteById(@Param() id: string): string {
+    return `delete ${id}`;
+  }
+
 }
